1b25f8ae5ffc04a5c2a11d0f816066192e9b4819
ajradtke <100101271+ajradtke@users.noreply.github.com>
2022-05-18 19:10:09 -0400

    added daily challenge for day 2 content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Single line comment
 /* Multi-line comment
 that carries to the line below it */
+myVar = "Supercalifragilisticexpialidocious";
+myNum = 11;
+myBigInt = 395n;
 
 console.log("Aaron Radtke");
 console.log("Steak and Potatoes");
+
+console.log(myVar.length);
+console.log(11);
+console.log(395n);
+console.log(typeof myNum, myNum);
+console.log(typeof myBigInt, myBigInt);
